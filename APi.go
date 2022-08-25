676ef745fa8f999eb616261f7ecd09984423b7e8
Sementsov mikhail <semmisha@yandex.ru>
2022-08-26 01:28:21 +0300

    url, to additional struct element

--- APi.go
+++ APi.go
@@ -9,18 +9,22 @@ import (
 )
 
 type Message struct {
+	URL     string `json:"url"`
 	Channel string `json:"channel"`
 	Text    string `json:"text"`
 }
 
-func NewConnection(channel string) *Message {
-	return &Message{Channel: channel}
+func NewConnection(channel string, url string) *Message {
+	return &Message{
+		Channel: channel,
+		URL:     url,
+	}
 }
 
 func (m *Message) Write(p []byte) (n int, err error) {
-	fmt.Println("test ", p)
 
 	m.Text = bytes.NewBuffer(p).String()
+
 	marshMellow, err := json.Marshal(m)
 	if err != nil {
 		fmt.Println(err)
@@ -28,7 +32,7 @@ func (m *Message) Write(p []byte) (n int, err error) {
 	}
 	client := http.Client{}
 
-	resp, err := client.Post("http://127.0.0.1:9999/PostMessage", "application/json ", bytes.NewBuffer(marshMellow))
+	resp, err := client.Post(m.URL, "application/json ", bytes.NewBuffer(marshMellow))
 	if err != nil {
 
 		fmt.Println("error")
